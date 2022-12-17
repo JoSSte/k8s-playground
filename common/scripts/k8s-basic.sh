fc49c499bbc1ccd034698bbaaa48219ed9cfa613
Jonas Stumph Stevnsvig <jonas@stevnsvig.com>
2022-12-17 22:33:52 +0100

    added readme

--- common/scripts/k8s-basic.sh
+++ common/scripts/k8s-basic.sh
@@ -13,7 +13,7 @@ overlay
 br_netfilter
 EOF
 
->&2 echo "\n\n * * * * * * * * * * * * * * * * * * 1 * * * * * * * * * * * * * * * * * * \n\n"
+>&2 echo -e "\n * * * * * * * * * * * * * * * * * *    1    * * * * * * * * * * * * * * * * * * \n"
 
 sudo modprobe overlay
 sudo modprobe br_netfilter
@@ -23,31 +23,34 @@ net.bridge.bridge-nf-call-iptables = 1
 net.ipv4.ip_forward = 1
 EOF
 
->&2 echo "\n\n * * * * * * * * * * * * * * * * * * 2 * * * * * * * * * * * * * * * * * * \n\n"
+>&2 echo -e "\n * * * * * * * * * * * * * * * * * *    2    * * * * * * * * * * * * * * * * * * \n"
 
 sudo sysctl --system
 sudo apt-get install -qy curl gnupg2 software-properties-common apt-transport-https ca-certificates
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/docker.gpg
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get install -qy containerd.io containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
+#sudo apt-get update
+sudo apt-get install -qy containerd.io
+containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
 sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
 
->&2 echo "\n\n * * * * * * * * * * * * * * * * * * 3 * * * * * * * * * * * * * * * * * * \n\n"
+>&2 echo -e "\n * * * * * * * * * * * * * * * * * *    3    * * * * * * * * * * * * * * * * * * \n"
 
 sudo systemctl restart containerd
 sudo systemctl enable containerd
 # consider updating this according to the answers here: https://stackoverflow.com/questions/68992799/warning-apt-key-is-deprecated-manage-keyring-files-in-trusted-gpg-d-instead
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
-sudo apt-get update
+#sudo apt-get update
 sudo apt-get install -qy kubelet kubeadm kubectl
 
->&2 echo "\n\n * * * * * * * * * * * * * * * * * * 4 * * * * * * * * * * * * * * * * * * \n\n"
+>&2 echo -e "\n * * * * * * * * * * * * * * * * * *    4    * * * * * * * * * * * * * * * * * * \n"
 
 sudo apt-mark hold kubelet kubeadm kubectl
 sudo swapoff -a
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+sudo rm /etc/containerd/config.toml
+sudo systemctl restart containerd
 sudo kubeadm init --control-plane-endpoint=$SERVER_NAME
 sudo shutdown -r now
 mkdir -p $HOME/.kube
@@ -56,7 +59,7 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl cluster-info
 kubectl get nodes
 
->&2 echo "\n\n * * * * * * * * * * * * * * * * * * 5 * * * * * * * * * * * * * * * * * * \n\n"
+>&2 echo -e "\n * * * * * * * * * * * * * * * * * *    5    * * * * * * * * * * * * * * * * * * \n"
 
 sudo apt-get update
 sudo apt-get upgrade -qy
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Vagrant VMS with Kubernetes
+
+In order to experiement with kubernetes (k8s) I have created a couple of vagrantfiles to exeperiment.
+
+They are based on [this guide](https://www.linuxtechi.com/install-kubernetes-on-ubuntu-22-04/).
+
+The reasons for not just creating the VMs are three-fold:
+1. The images are not easily recreatable in case I mess something up
+1. The images take up more space than a couple of script files
+1. 
