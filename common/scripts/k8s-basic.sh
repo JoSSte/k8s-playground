15232b241462d4601eb2eccd556ce854fd575391
Jonas Stumph Stevnsvig <jonas@stevnsvig.com>
2022-12-17 20:32:44 +0100

    added some debugging and ensured `-qy` flags are sent with `apt-get install`

--- common/scripts/k8s-basic.sh
+++ common/scripts/k8s-basic.sh
@@ -2,7 +2,7 @@
 
 # https://www.linuxtechi.com/install-kubernetes-on-ubuntu-22-04/
 
-sudo apt-get install screenfetch
+sudo apt-get install -qy screenfetch
 echo "if [ -f /usr/bin/screenfetch ]; then screenfetch; fi" >> ~/.bashrc
 sudo apt-get update
 sudo apt-get upgrade -qy
@@ -13,6 +13,8 @@ overlay
 br_netfilter
 EOF
 
+>&2 echo "\n\n * * * * * * * * * * * * * * * * * * 1 * * * * * * * * * * * * * * * * * * \n\n"
+
 sudo modprobe overlay
 sudo modprobe br_netfilter
 sudo tee /etc/sysctl.d/kubernetes.conf <<EOF
@@ -21,19 +23,28 @@ net.bridge.bridge-nf-call-iptables = 1
 net.ipv4.ip_forward = 1
 EOF
 
+>&2 echo "\n\n * * * * * * * * * * * * * * * * * * 2 * * * * * * * * * * * * * * * * * * \n\n"
+
 sudo sysctl --system
-sudo apt-get install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
+sudo apt-get install -qy curl gnupg2 software-properties-common apt-transport-https ca-certificates
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/docker.gpg
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
-sudo apt-get install -y containerd.io containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
+sudo apt-get install -qy containerd.io containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
 sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+
+>&2 echo "\n\n * * * * * * * * * * * * * * * * * * 3 * * * * * * * * * * * * * * * * * * \n\n"
+
 sudo systemctl restart containerd
 sudo systemctl enable containerd
+# consider updating this according to the answers here: https://stackoverflow.com/questions/68992799/warning-apt-key-is-deprecated-manage-keyring-files-in-trusted-gpg-d-instead
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
 sudo apt-get update
-sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-get install -qy kubelet kubeadm kubectl
+
+>&2 echo "\n\n * * * * * * * * * * * * * * * * * * 4 * * * * * * * * * * * * * * * * * * \n\n"
+
 sudo apt-mark hold kubelet kubeadm kubectl
 sudo swapoff -a
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
@@ -44,3 +55,8 @@ sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl cluster-info
 kubectl get nodes
+
+>&2 echo "\n\n * * * * * * * * * * * * * * * * * * 5 * * * * * * * * * * * * * * * * * * \n\n"
+
+sudo apt-get update
+sudo apt-get upgrade -qy
