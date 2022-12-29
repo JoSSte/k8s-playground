018918e542c0582fd6d0be95043a1f64a49a7e65
Jonas Stumph Stevnsvig <jonas@stevnsvig.com>
2022-12-29 12:58:39 +0100

    fix script to not include slashes

--- box/deploy.sh
+++ box/deploy.sh
@@ -1,8 +1,9 @@
 BOX_ID="JoSSte/k8sBasic"
+FILENAME=`echo $BOX_ID | sed -r 's/\//_/g'`
 vagrant destroy -f
 set -e
 vagrant up # Start the box and run any defined scripts
-vagrant package --output ${BOX_ID}.box # Store the virtual machine with all the software installed into a box file.
-vagrant box add --force ${BOX_ID} ${BOX_ID}.box # Add the box to the vagrant repo using the given id.
+vagrant package --output ${FILENAME}.box # Store the virtual machine with all the software installed into a box file.
+vagrant box add --force "${BOX_ID}" ${FILENAME}.box # Add the box to the vagrant repo using the given id.
 vagrant destroy -f # Stop the vagrant instance
-rm ${BOX_ID}.box # Remove the file 
+rm ${FILENAME}.box # Remove the file 
