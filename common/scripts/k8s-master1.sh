f115349640d59cd3075b6a1f4e0eea9f81f741b1
Jonas Stumph Stevnsvig <jonas@stevnsvig.com>
2023-01-09 14:08:05 +0100

    tweaking file permissions

--- common/scripts/k8s-master1.sh
+++ common/scripts/k8s-master1.sh
@@ -3,5 +3,10 @@ export KUBECONFIG=/home/vagrant/.kube/config
 echo -e "\nexport KUBECONFIG=$KUBECONFIG\n" >> /home/vagrant/.bashrc
 mkdir /home/vagrant/.kube 2> /dev/null
 sudo k3s kubectl config view --raw > "$KUBECONFIG"
+sudo chown vagrant:vagrant "$KUBECONFIG"
+sudo chmod 600 "$KUBECONFIG"
+
+# copy conifg to shared folder for worker nodes
+
 cp $KUBECONFIG /tmp/shared/config
-chmod 600 "$KUBECONFIG"
+sed -i "s#127.0.0.1#/$SERVER_NAME#" /tmp/shared/config
